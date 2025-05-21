89d354ac5895361a710045aa39461079bd3da1b5
asddssaa28 <zxccxxzz28@gmail.com>
2025-05-21 17:43:36 +0800

    Add files via upload

---
+++ pfa-interview-site-complete/components/ui/button.tsx
@@ -0,0 +1,12 @@
+
+export function Button({ children, disabled, onClick, className }: any) {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`px-4 py-2 rounded-xl bg-gray-800 text-white ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
